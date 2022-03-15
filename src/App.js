7631536da5b2ea05b45e0f96e1827c621b95a9e0
nabarun123 <nabarunjyotiboruah@gmail.com>
2022-03-15 14:35:00 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Login from "./componants/login/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
